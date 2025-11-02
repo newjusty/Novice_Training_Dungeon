04b93d8b21c598a540efed8c5937bfc1906ea5ea
newjsty <newsweettime@gmail.com>
2025-11-02 12:38:01 +0700

    setup game

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/https://github.com/newjusty/Novice_Training_Dungeon.git/' // **เปลี่ยนตรงนี้เป็นชื่อ Repository ของคุณ**
+    : '/',
+};
