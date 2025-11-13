66314c8fb79c2c3255aecbd8d7624add20320f8e
newjsty <newsweettime@gmail.com>
2025-11-13 21:39:01 +0700

    update_config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/https://github.com/newjusty/Novice_Training_Dungeon.git/' // **เปลี่ยนตรงนี้เป็นชื่อ Repository ของคุณ**
+    ? '/Novice_Training_Dungeon/' // **เปลี่ยนตรงนี้เป็นชื่อ Repository ของคุณ**
     : '/',
 };
